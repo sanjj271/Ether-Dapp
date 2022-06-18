b5400fd41e642625afcfd3371b3b56a5c169d1a4
Sanjana gogte <sanj27272@gmail.com>
2022-06-18 18:38:48 +0530

    second commit

--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require('./compile');
+
+
+const provider = new HDWalletProvider(
+    'cousin axis rubber cushion expose mix denial claim dilemma plastic oyster mind', 
+    'https://rinkeby.infura.io/v3/baa7ff9dc3894d3cbdab7fdf8801015e'
+)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts()
+    console.log('Attempting to deploy from', accounts[0])
+
+    const result = await new web3.eth.Contract(abi)
+    .deploy({data: evm.bytecode.object, arguments : ['Hi there']})
+    .send({gas: '1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+}
+deploy()
