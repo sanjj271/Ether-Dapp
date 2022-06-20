73967340744494733c0eec5c55545dc3c0a94b2c
Sanjana gogte <sanj27272@gmail.com>
2022-06-20 14:49:58 +0530

    last commit

--- compile.js
+++ compile.js
@@ -2,27 +2,29 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
- 
+
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
- 
+
 const input = {
-    language: 'Solidity',
-    sources: {
-        'Inbox.sol': {
-            content: source,
-        },
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: source,
     },
-    settings: {
-        outputSelection: {
-            '*': {
-                '*': ['*'],
-            },
-        },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
     },
+  },
 };
- 
- 
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-    'Inbox.sol'
+const a = JSON.stringify(input)
+const b = solc.compile(a)
+console.log(b)
+const c = JSON.parse(b)
+module.exports = c.contracts[
+  'Inbox.sol'
 ].Inbox;
--- deploy.js
+++ deploy.js
@@ -1,23 +1,25 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+
 const { abi, evm } = require('./compile');
 
+provider = new HDWalletProvider(
+  'cousin axis rubber cushion expose mix denial claim dilemma plastic oyster mind',
+  'https://rinkeby.infura.io/v3/baa7ff9dc3894d3cbdab7fdf8801015e'
+);
 
-const provider = new HDWalletProvider(
-    'cousin axis rubber cushion expose mix denial claim dilemma plastic oyster mind', 
-    'https://rinkeby.infura.io/v3/baa7ff9dc3894d3cbdab7fdf8801015e'
-)
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts()
-    console.log('Attempting to deploy from', accounts[0])
+  const accounts = await web3.eth.getAccounts();
 
-    const result = await new web3.eth.Contract(abi)
-    .deploy({data: evm.bytecode.object, arguments : ['Hi there']})
-    .send({gas: '1000000', from: accounts[0]});
+  console.log('Attempting to deploy from account', accounts[0]);
 
-    console.log('Contract deployed to', result.options.address);
-    provider.engine.stop();
-}
-deploy()
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
